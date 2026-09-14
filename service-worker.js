c26b9b46a2fb7572f6a22d5495818d14ccb95736
jinan <bigstarcmc@naver.com>
2026-09-14 19:33:21 +0900

    Force service worker refresh and network-first cache policy

--- service-worker.js
+++ service-worker.js
@@ -33,21 +33,25 @@ self.addEventListener('fetch', (event) => {
     return;
   }
 
-  event.respondWith(
-    caches.match(event.request).then((cached) => {
-      if (cached) {
-        return cached;
-      }
+  const requestUrl = new URL(event.request.url);
+  if (requestUrl.origin !== self.location.origin) {
+    return;
+  }
 
-      return fetch(event.request)
-        .then((response) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        if (response && response.ok) {
           const responseClone = response.clone();
           caches.open(CACHE_NAME).then((cache) => {
             cache.put(event.request, responseClone);
           });
-          return response;
-        })
-        .catch(() => caches.match('./index.html'));
-    })
+        }
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request)
+          .then((cached) => cached || caches.match('./index.html'));
+      })
   );
 });
